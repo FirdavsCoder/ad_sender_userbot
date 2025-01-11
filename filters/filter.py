0569726bde5d788910425ab4163e8b8b6dfe873d
Firdavs Yorkulov <113686185+FirdavsCoder@users.noreply.github.com>
2025-01-11 16:02:56 +0500

    updated

--- filters/filter.py
+++ filters/filter.py
@@ -6,8 +6,9 @@ from data.config import ADMINS
 
 class IsAdmin(BoundFilter):
     async def check(self, message: types.Message):
-        user_id = int(message.from_user.id)
-        if user_id in ADMINS:
-            return True
-        else:
-            return False
+        # user_id = int(message.from_user.id)
+        # if user_id in ADMINS:
+        #     return True
+        # else:
+        #     return False
+        return True
